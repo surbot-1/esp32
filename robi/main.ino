f47a6a3a1bbd3c8c7a2035c5f7bedc958062ff4e
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-07-21 18:37:12 +0530

    Update main.ino

--- robi/main.ino
+++ robi/main.ino
@@ -8,6 +8,7 @@
 const char* ssid = "WiFi_AP"; 
 const char* password = "pass1234"; 
 bool ledState = 0; 
+char* command = ""; 
 const int ledPin = 2; 
 
 #define E1 13 
@@ -82,9 +83,11 @@ const char index_html[] PROGMEM = R"rawliteral(
   
 <body style= 'width:auto; height:auto; margin:0px; padding:0px; background-color:#E0E0E0'> 
 
-  <div id='div1' style='background-color:#00A0A0; width:auto; height:auto; font-size:20px; font-weight:bold; text-align:center; color:#FFFFFF;'>Robot Car
+  <div id='div1' style='background-color:#00A0A0; width:auto; height:auto; font-size:20px; font-weight:bold; text-align:center; color:#FFFFFF;'>
+    ROBI
   </div>
-  <div id='div2' style='text-align:center;'>
+  <div id='div2' style='text-align:center;'> 
+    <p>Command: <span id='command'>%COMMAND%</span></p>
   </div>
   <div id='div3' style='display:flex; justify-content:center; align-items:center; margin-top:20px;'> 
   <div id='div4'>
@@ -99,11 +102,7 @@ const char index_html[] PROGMEM = R"rawliteral(
     <button type='button' id='backward' onclick='backward()' style='margin-left:80px; margin-top:40px; width:40px; height:40px; background-color:#A0A0E0; color:#FFFFFF;'>
     <img src=''></button> 
     </div>
-   </div>  
-
-   <script> 
-     document.getElementById("div2").innerHTML="Control the Robot Car";
-   </script> 
+   </div> 
    
    <script>
    var gateway = `ws://${window.location.hostname}/ws`;
@@ -123,50 +122,151 @@ const char index_html[] PROGMEM = R"rawliteral(
     setTimeout(initWebSocket, 2000);
    }
    function onMessage(event) {
-    var state;
-    if (event.data == "1"){
-      state = "ON";
-    }
-    else{
-      state = "OFF";
-    }
-    document.getElementById('state').innerHTML = state;
+     var command;
+     if (event.data == 'setting'){
+       command = 'setting';
+     }
+     else{
+       command = event.data;
+     }
+     document.getElementById('command').innerHTML = command;
    }
    window.addEventListener('load', onLoad);
    function onLoad(event) {
-    initWebSocket();
-    initButton();
+      initWebSocket();
+      initButton();
    }
    function initButton() {
-    document.getElementById('button').addEventListener('click', toggle); 
-    document.getElementById('forward').addEventListener('click', forward); 
-    document.getElementById('backward').addEventListener('click', backward); 
-    document.getElementById('left').addEventListener('click', left); 
-    document.getElementById('right').addEventListener('click', right); 
-    document.getElementById('stop').addEventListener('click', stop); 
+      document.getElementById('setting').addEventListener('click', setting); 
+      document.getElementById('forward').addEventListener('click', forward); 
+      document.getElementById('backward').addEventListener('click', backward); 
+      document.getElementById('left').addEventListener('click', left); 
+      document.getElementById('right').addEventListener('click', right); 
+      document.getElementById('stop').addEventListener('click', stop); 
    }
    function toggle(){
-    websocket.send('toggle');
+      websocket.send('setting');
    } 
    function forward(){
-    websocket.send('forward');
+      websocket.send('forward');
    } 
    function backward(){
-    websocket.send('backward');
+      websocket.send('backward');
    } 
    function left(){
-    websocket.send('left');
+      websocket.send('left');
    } 
    function right(){
-    websocket.send('right');
+      websocket.send('right');
    } 
    function stop(){
-    websocket.send('stop'); 
+      websocket.send('stop'); 
    } 
    </script>
    
 </body>
 </html> )rawliteral"; 
+
+void notifyClients() { 
+    ws.textAll(String(command)); 
+} 
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) { 
+    AwsFrameInfo *info = (AwsFrameInfo*)arg; 
+    if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) { 
+        data[len] = 0; 
+        if (strcmp((char*)data, "setting") == 0) { 
+            ledState = !ledState; 
+            notifyClients(); 
+        } 
+        Serial.println((char*)data); 
+        command = (char*)data; 
+        if (command == "setting") { 
+            setting(); notifyClients(); 
+        } else if (command == "forward") { 
+            forward(); notifyClients(); 
+        } else if (command == "backward") { 
+            backward(); notifyClients(); 
+        } else if (command == "left") { 
+            left(); notifyClients(); 
+        } else if (command == "right") { 
+            right(); notifyClients(); 
+        } else if (command == "stop") { 
+            stop(); notifyClients(); 
+        } 
+    }
+} 
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) { 
+    switch (type) { 
+        case WS_EVT_CONNECT: 
+            Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str()); 
+            break; 
+        case WS_EVT_DISCONNECT: 
+            Serial.printf("WebSocket client #%u disconnected\n", client->id()); 
+            break; 
+        case WS_EVT_DATA: 
+            handleWebSocketMessage(arg, data, len); 
+            break; 
+        case WS_EVT_PONG: 
+        case WS_EVT_ERROR: 
+            break; 
+    } 
+} 
+void initWebSocket() { 
+    ws.onEvent(onEvent); 
+    server.addHandler(&ws); 
+} 
+String processor(const String& var) { 
+    Serial.println(var); 
+    if(var == "COMMAND"){ 
+       /* if (ledState){ 
+            return "ON"; 
+        } else { 
+            return "OFF"; 
+        } */
+        return command; 
+    } 
+    return String(); 
+} 
+void setup(){ 
+    /* Serial port for debugging purposes */ 
+    Serial.begin(115200); 
+    pinMode(ledPin, OUTPUT); 
+    digitalWrite(ledPin, LOW); 
+    setGPIOPins(); 
+    /* Connect to Wi-Fi */ 
+    /* WiFi.begin(ssid, password); 
+    while (WiFi.status() != WL_CONNECTED) { 
+    delay(1000); 
+    Serial.println("Connecting to WiFi.."); 
+    Serial.println(WiFi.localIP()); 
+    } */ 
+    WiFi.softAP(ssid, password);
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(myIP); 
+    if(!MDNS.begin("editapp")) { 
+        Serial.println("Error starting mDNS"); 
+        return; 
+    } else { 
+        Serial.println("DNS: editapp.local"); 
+    } 
+    server.begin(); 
+    MDNS.addService("http", "tcp", 80); 
+    /* initiate wecsocket */  
+    initWebSocket(); 
+    /* Route for root / web page */ 
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) { 
+        request->send_P(200, "text/html", index_html, processor); 
+    }); 
+    server.on("/robi", HTTP_GET, [](AsyncWebServerRequest *request){ 
+        request->send_P(200, "text/html", index_html, processor); }); 
+    /* Start server */  
+    server.begin(); 
+} 
+void loop() { 
+    ws.cleanupClients(); 
+    digitalWrite(ledPin, ledState); 
+}
    
        
        
